b8463ef3217136d43a9a9fbb572c4c71b9ebe74e
doop-k <1768628555@qq.com>
2019-10-18 15:04:47 +0800

    register user/python flask/web

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask,render_template
 from flask import request
-
+import sqlite3
 app = Flask(__name__)
+conn=sqlite3.connect('test.db')
+c=conn.cursor()
+c.execute('''create table userInfo(email text,password text);''')
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/album')
 def album():
@@ -16,13 +20,12 @@ def album():
 def loginAsp():
     return render_template('login.html')
 
-@app.route('/signIn',method=['POST'])
+@app.route('/signIn',methods=['POST'])
 def signIn():
     user=request.form('email')
     password=request.form('password')
-
+    c.execute('insert into userInfo("'+user+'","'+password+'")')
     return '<h1>done </h1>'+request.form['email']
-
 
 if __name__ =='__main__':
     app.run()
